4e1cca65a7b0be08e185abfeab536a40c607f033
noisyboy25 <dj402and@gmail.com>
2019-08-31 07:29:53 +0300

    Add fs.mkdir

--- build.js
+++ build.js
@@ -2,19 +2,24 @@ const fs = require('fs');
 const archiver = require('archiver');
 const path = require('path');
 
-console.log(process.argv);
+const defaultDir = 'dist';
+const fileName = 'noisyboy_datapack.zip';
+const outDir = process.argv[2] || defaultDir;
+const out = path.join(__dirname, outDir, fileName);
 
-const defaultOut = 'dist';
-const outDir = process.argv[2] || defaultOut;
-const out = path.join(__dirname, outDir, 'noisyboy_datapack.zip');
-
-const stream = fs.createWriteStream(out);
 console.log(out);
-const archive = archiver('zip');
 
-archive
-    .directory('src', false)
-    .on('error', (err) => console.log(err))
-    .on('finish', () => console.log('Building finished'))
-    .pipe(stream);
-archive.finalize();
+fs.mkdir(path.join(__dirname, outDir), (err) => {
+    if (err && err.code !== 'EEXIST') console.log(err);
+    else {
+        const stream = fs.createWriteStream(out);
+        const archive = archiver('zip');
+        archive
+            .directory('src', false)
+            .on('error', (err) => console.log(err))
+            .on('finish', () => console.log('Building finished'))
+            .pipe(stream);
+        archive.finalize();
+    }
+});
+
